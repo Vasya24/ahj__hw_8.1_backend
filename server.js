046840ca8d8c98ae72a2947718e5ec10c4bbc901
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-10-21 15:12:40 +0300

    Server done

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const http = require('http');
 const Koa = require('koa');
 const koaBody = require('koa-body');
+const Router = require('koa-router');
+const { streamEvents } = require('http-event-stream');
 const uuid = require('uuid');
+const router = new Router();
 const app = new Koa();
 
 app.use(koaBody({
     urlencoded: true,
     multipart: true,
+    json: true,
 }));
 
 app.use(async (ctx, next) => {
@@ -41,22 +45,92 @@ app.use(async (ctx, next) => {
     }
   });
 
+  function initDate() {
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear().toString().slice(2);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    return `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds} ${day < 10 ? '0' : ''}${day}.${month < 10 ? '0' : ''}${month}.${year}`;
+  }
 
-  //Здесь будет код
+  let count = 0;
+  const reports = [];
 
-
-
-
-  app.use(async (ctx) => {
-    const { method } = ctx.request.query;
-    switch (method) {
-      case 'greeting':
-        ctx.response.body = 'Hey there...Welcome';
-        return;
-      default:
-        ctx.response.status = 404;
-    }
+  reports.push({
+    event: 'comment',
+    data: JSON.stringify({
+      type: 'start',
+      message: 'Игра началась',
+      date: initDate()
+    }),
+    id: uuid.v4()
   });
+
+  const interval = setInterval(() => {
+    const events = [
+      {
+        type: 'action',
+        message: 'Идёт перемещение мяча по полю, игроки и той, и другой команды активно пытаются атаковать'
+      },
+      {
+        type: 'freekick',
+        message: 'Нарушение правил, будет штрафной удар'
+      },
+      {
+        type: 'goal',
+        message: 'Отличный удар! И Г-О-О-О-Л!'
+      }
+    ];
+
+    let index = 0;
+    const random = Math.floor(Math.random() * 10);
+    if (random < 1) {
+      index = 2;
+    } else if (random < 5) {
+      index = 1;
+    }
+
+    reports.push({
+      event: 'comment',
+      data: JSON.stringify({
+        type: events[index].type,
+        message: events[index].message,
+        date: initDate()
+      }),
+      id: uuid.v4()
+    });
+
+    count++;
+    if (count > 50) clearInterval(interval);
+  }, 3000);
+
+
+  router.get('/sse', async (ctx) => {
+    streamEvents(ctx.req, ctx.res, {
+      async fetch(lastEventId) {
+        console.log(lastEventId);
+        return [];
+      },
+      stream(sse) {
+        let i = 0;
+        const interval = setInterval(() => {
+          if (reports.length > i) {
+            sse.sendEvent(reports[i]);
+            i++;
+          }
+          if (i > 50) clearInterval(interval);
+
+        }, 3000);
+        return () => clearInterval(interval);
+      }
+    });
+    ctx.respond = false;
+  });
+
+  app.use(router.routes()).use(router.allowedMethods());
 
   const port = process.env.PORT || 4242;
   http.createServer(app.callback()).listen(port);
